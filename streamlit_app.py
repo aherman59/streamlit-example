b84008d56b24d1e2136015fcd4881b1040e3c21c
aherman59 <herman.antoine@gmail.com>
2023-03-15 20:17:43 +0100

    add departement choices

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,14 @@ import pandas as pd
 import streamlit as st
 import requests
 
+def get_departements():
+    url = f"https://geo.api.gouv.fr/departements/{departement}/communes"
+    response = requests.get(url)
+    if response.status_code == 200:
+        communes = response.json()
+        return communes
+    return None
+
 def get_communes(departement):
     url = f"https://geo.api.gouv.fr/departements/{departement}/communes"
     response = requests.get(url)
@@ -10,11 +18,13 @@ def get_communes(departement):
         return communes
     return None
 
+departements = get_departements()
+
 st.title("Erosion du trait de côte")
 
-departement = st.selectbox("Choix du département", ["06", "59", "33"])
+departement = st.selectbox("Choix du département", [d["nom"] for d in departements])
 
-communes = get_communes(departement)
+communes = get_communes([d["code"] for d in departements if d["nom"] == departement][0])
 
 commune = st.selectbox("Choix de la commune", [c["nom"] for c in communes])
 
