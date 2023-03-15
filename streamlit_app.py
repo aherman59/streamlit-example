444aa0490ae9eb45aad45a27c8da6aa38de8467f
aherman59 <herman.antoine@gmail.com>
2023-03-15 20:01:53 +0100

    test appel api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,17 @@
-from collections import namedtuple
 import pandas as pd
 import streamlit as st
+import requests
 
+def get_communes(departement):
+    url = f"https://geo.api.gouv.fr/departements/{departement}/communes"
+    response = requests.get(url)
+    if response.code == 200:
+        communes = response.json()
+        return communes
+    return None
 
 st.title("Application test - Commune")
 
 st.subheader("Choix de la commune")
+
+st.text(get_communes("06"))
